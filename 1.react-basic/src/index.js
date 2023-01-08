d34d7cf5b6356382efc7cce98aaafe304a157a10
skuxyx <skubaltz@naver.com>
2023-01-08 23:49:52 +0900

    react study

--- 1.react-basic/src/index.js
+++ 1.react-basic/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// App.js import
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Public => 정적파일 ( image, html 등 )
+// src => React 작업 폴더.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // 애플리케이션 내 잠재적인 문제를 알아내기 위한 도구로 활용.
   <React.StrictMode>
+    {/* App ( app.js ) 코드를 화면에 출력 시켜 줌. */}
     <App />
   </React.StrictMode>
 );
