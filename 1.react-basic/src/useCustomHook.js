b01173a506a3400a1ae2626dbe6f4f7a6a8df84c
skuxyx <skubaltz@naver.com>
2023-01-26 23:24:43 +0900

    input customhook

--- 1.react-basic/src/useCustomHook.js
+++ 1.react-basic/src/useCustomHook.js
@@ -3,6 +3,9 @@ import { useState } from 'react';
 // custom hook 을 사용하기 위해 useInput import.
 import useInput from './hooks/useInput';
 
+// 1. 커스텀 훅 내부에서 다른 Hook ( useEffect, useState ,,,)을 사용할 수 있음.
+// 2. 각 컴포넌트 마다 독립적으로 사용 => 재사용성 ↑
+
 // 커스텀 훅(Custom Hook)을 정의 할 때 함수 앞에 use를 붙여야 함 ( useInput, useFatch,, )
 export default function useCustomHook() {
   // const [yourName, setYourName] = useState('');
